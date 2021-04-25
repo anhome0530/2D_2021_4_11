c8477517b3e10df4702ee5eb0fd904fa6a52c001
anhome0530 <82298591+anhome0530@users.noreply.github.com>
2021-04-25 17:00:00 +0800

    程式3

---
+++ ROCKMAN 2D/Assets/scripts/player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 10.5f;
+    [Header("跳越高度"), Range(0, 3000)]
+    public int height = 100;
+    [Header("血量"), Range(0, 200)]
+    public float blood = 100;
+    [Header("是否在地板上"), Tooltip ("儲存角色是否在地板上")]
+    public bool onLand;
+    [Header("子彈"), Tooltip("角色要發射的子彈物件")]
+    public GameObject bullet;
+    [Header("子彈生成點"), Tooltip("生成子彈的位子")]
+    public Transform bulletPoint;
+    [Range(0, 5000)]
+    public int bulletSpeed = 800;
+    [Header("開槍音效"), Tooltip("開槍的聲音")]
+    public AudioClip bulletSound;
+
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+
+
+
+
+
+
+
+
+
+}
