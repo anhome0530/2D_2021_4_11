2e353cab9302d8ab48c3b5e775587f39a34d375a
anhome0530 <82298591+anhome0530@users.noreply.github.com>
2021-06-27 16:52:36 +0800

    web

---
+++ ROCKMAN 2D/Assets/scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    #region 欄位
+    [Header("移動速度"), Range(0, 100)]
+    public float speed = 1f;
+    [Header("攻擊力"), Range(0, 100)]
+    public float attack = 10f;
+    [Header("血量"), Range(0, 1000)]
+    public float hp = 100f;
+
+    private Transform player;
+    private Rigidbody2D rig;
+    private Animator ani;
+    #endregion
+
+    #region
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+
+        player = GameObject.Find("玩家").transform;
+
+    }
+    #endregion
+}
